4c4472188985e4d568ce33f166727faaf812edac
Sorracha-A <64011628@kmitl.ac.th>
2023-05-23 21:02:54 +0700

    goal page add

---
+++ src/loginPageModule.py
@@ -0,0 +1,3 @@
+Error: File 'loginPage.ui' is not valid
+
+while executing '/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/PySide6/Qt/libexec/uic -g python loginPage.ui'
